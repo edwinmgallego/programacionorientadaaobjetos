7b7c93c7b3a163fb685edda1d122a5f5fad2335e
edwinmgallego <ing.edwin.gallego@gmail.com>
2022-04-04 16:58:34 -0500

    recordando como se asigna  una variable  en   java

--- introduccionajavaplatziv3/src/com/company/Main.java
+++ introduccionajavaplatziv3/src/com/company/Main.java
@@ -3,6 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// recordando variables  ,  declarando la variable speed.
+
+        int speed;
+        //asiganamos  valor  a la variable
+        speed=10;
+        System.out.println(speed);
     }
 }
