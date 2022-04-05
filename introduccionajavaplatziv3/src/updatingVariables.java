cc8fea7879823792cb5ffa34d374d26b01379b26
edwinmgallego <ing.edwin.gallego@gmail.com>
2022-04-05 11:40:02 -0500

    recordando como se asigna  una variable  en   java y   haciendo  operaciones  aritmeticas

--- introduccionajavaplatziv3/src/updatingVariables.java
+++ introduccionajavaplatziv3/src/updatingVariables.java
@@ -3,7 +3,19 @@ public class updatingVariables {
         int  salary = 1000;
 
         //bono 200
+        salary = salary +200;
 
+        System.out.println(salary);
+        //le  quitamos  50 pesos  de pension
 
+        salary= salary - 50;
+
+        System.out.println(salary);
+
+        //bono de   2  horas  30 pesos  cada  una y  ademas  tiene  un cupon  de comida que   vale 45 pesos
+
+        salary = salary+( 30*2) -45;
+
+        System.out.println(salary);
     }
 }
