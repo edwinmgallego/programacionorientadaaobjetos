2109dc7e7a14f8914b1979b5e50d9f5e5af42b46
edwinmgallego <ing.edwin.gallego@gmail.com>
2022-04-05 15:21:35 -0500

    empezando estructuras de datos

---
+++ DataStructuresJava/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int n=0;
+
+        long nL= 123456789;
+        double nD =123.123;
+        float nF =123.456F;
+
+        var salary = 1000;
+        //pension
+
+        var pension = salary* 0.03;
+        System.out.println(salary);
+        System.out.println(pension);
+
+        var  total = salary - pension;
+
+        System.out.println(total);
+
+
+
+    }
+}
